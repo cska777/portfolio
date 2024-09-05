bb218dbc426606ed91b15d8fe05d7f01b6c1831b
Christophe Carbin <blakehziir@gmail.com>
2024-09-05 20:40:47 +0200

    Updates

--- server/server-reference-manifest.js
+++ server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"sX1DoAOFT1ezjLGmUjpCM8vWXO9vlS/kYv7ltzQbyfU=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"ZutNMshqpZKbr+0rajg2Lx/Lr/tlWPcx5qi7N8SR4o0=\"}"
